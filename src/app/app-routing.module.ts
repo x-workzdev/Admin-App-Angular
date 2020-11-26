2b297c9a1d5520efdd3088c8b1c6821b4f4470c7
= <=>
2020-11-26 23:24:41 +0530

    upadted admin app with all components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { ViewClientRegistersComponent } from './view-client-registers/view-client-registers.component';
+import { ViewticketsComponent } from './view-tickets/view-tickets.component';
+import { ViewGadgetsComponent } from './view-gadgets/view-gadgets.component';
+
+const routes: Route[] = [
+  {path:'', redirectTo:'/login', pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'signup', component:SignupComponent},
+  {path : 'profile',component : ProfileComponent},
+  {path : 'clients',component : ViewClientRegistersComponent},
+  {path : 'tickets',component : ViewticketsComponent},
+  {path : 'gadgets',component : ViewGadgetsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {DataTablesModule} from 'angular-datatables';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from './services/login.service';
+import { ProfileComponent } from './profile/profile.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ViewClientRegistersComponent } from './view-client-registers/view-client-registers.component';
+import { ViewticketsComponent } from './view-tickets/view-tickets.component';
+import { ViewGadgetsComponent } from './view-gadgets/view-gadgets.component';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent,
+    ProfileComponent,
+    LogoutComponent,
+    ViewClientRegistersComponent,
+    ViewticketsComponent,
+    ViewGadgetsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    HttpClientModule,
+    DataTablesModule,
+    CommonModule,
+  ],
+  providers: [LoginService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
